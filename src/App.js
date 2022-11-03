e289ede90834fcc1e19cfc8bfa84dd6f7e2b516d
GiannaDavila <giannagiannadavila@gmail.com>
2022-11-03 12:12:55 -0400

    context example completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
-import './App.css';
+import { useState, createContext } from 'react'
 import Header from './components/Header';
 import Main from './components/Main';
+import './App.css';
+
+export const UserContext = createContext(null);
 
 function App() {
+  const [user, setUser] = useState('G')
   return (
-  <>
+  <UserContext.Provider value={{user, setUser}}>
   <Header/>
   <Main/> 
-  </>
+  </UserContext.Provider>
   );
 }
 
